9835f2d278fa7fe316904c148899a921817ba2a4
ArfinHasib <arfinhasib21375@gmail.com>
2023-10-13 22:10:12 +0600

    initialize setup

---
+++ src/components/Acomplishments/Acomplishments.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+import {
+  Section,
+  SectionDivider,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import { Box, Boxes, BoxNum, BoxText } from "./AcomplishmentsStyles";
+
+const data = [
+  { number: 10, text: "Open Source Projects" },
+  { number: 100, text: "Clients" },
+  { number: 200, text: "Completed Projects" },
+  // { number: 5000, text: "Github Stars" },
+];
+
+const Acomplishments = () => (
+  <Section>
+    <SectionTitle>Personal Achievements</SectionTitle>
+    <Boxes>
+      {data.map((card, index) => (
+        <Box key={index}>
+          <BoxNum>{`${card.number}+`}</BoxNum>
+          <BoxText>{card.text}</BoxText>
+        </Box>
+      ))}
+    </Boxes>
+    <SectionDivider />
+  </Section>
+);
+
+export default Acomplishments;
