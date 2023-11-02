da0978d9e35afbec036a5b42e5f11406a33b7195
ArfinHasib <arfinhasib21375@gmail.com>
2023-11-02 23:40:05 +0600

    Upadted buttons

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,30 +1,32 @@
 import React from 'react';
 
 import {
-  Section,
-  SectionText,
-  SectionTitle,
+   Section,
+   SectionText,
+   SectionTitle,
 } from '../../styles/GlobalComponents';
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <>
-    <Section row nopadding>
-      <LeftSection>
-        <SectionTitle main center>
-          Welcome To <br />
-          My Portfolio
-        </SectionTitle>
-        <SectionText>
-          Hello! I'm Arfin Hasib. A frontend Developer and UI/UX Designer. Ready
-          to help you out with your next big web development and UI/UX Design
-          for mobile and web.
-        </SectionText>
-        <Button onClick={() => (window.location = '#about')}>Hire Me</Button>
-      </LeftSection>
-    </Section>
-  </>
+   <>
+      <Section row nopadding>
+         <LeftSection>
+            <SectionTitle main center>
+               Welcome To <br />
+               My Portfolio
+            </SectionTitle>
+            <SectionText>
+               Hello! I'm Arfin Hasib. A frontend Developer and UI/UX Designer.
+               Ready to help you out with your next big web development and
+               UI/UX Design for mobile and web.
+            </SectionText>
+            <Button onClick={() => (window.location = '#about')}>
+               Learn More
+            </Button>
+         </LeftSection>
+      </Section>
+   </>
 );
 
 export default Hero;
