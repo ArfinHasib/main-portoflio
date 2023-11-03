dabd253e252f24076d011b649e936fe740f181c2
ArfinHasib <arfinhasib21375@gmail.com>
2023-11-03 21:22:39 +0600

    added title of the website

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,15 +1,21 @@
-import React from 'react'
+import React from 'react';
 
-import Footer from '../components/Footer/Footer'
-import Header from '../components/Header/Header'
-import { Container } from './LayoutStyles'
+import Footer from '../components/Footer/Footer';
+import Header from '../components/Header/Header';
+import { Container } from './LayoutStyles';
+import Head from 'next/head';
 
-export const Layout = ({children}) => {
-  return (
-    <Container>
-     <Header/>
-     <main>{children}</main> 
-     <Footer/>
-    </Container>
-  )
-}
+export const Layout = ({ children }) => {
+   return (
+      <>
+         <Head>
+            <title>Arfin Hasib | Frontend Developer & UI/UX Desginer</title>
+         </Head>
+         <Container>
+            <Header />
+            <main>{children}</main>
+            <Footer />
+         </Container>
+      </>
+   );
+};
