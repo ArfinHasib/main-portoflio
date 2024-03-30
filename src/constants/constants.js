b4690ada4631df9b6212557b9da60edd12b5c0d6
ArfinHasib <arfinhasib21375@gmail.com>
2024-03-30 11:42:21 +0600

    Updated Projects

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,50 +1,50 @@
 export const projects = [
-  {
-    title: 'Lamp Website',
-    description:
-      'Using HTML, CSS, & Javascript I have built this beautiful fluid, modern & responsive website. I have alsom used third party javascript animation library Scroll-Reveal and also icon library remix icon on the website.',
-    image: '/images/1.png',
-    tags: ['Html', 'CSS', 'Javascript'],
-    source: 'https://github.com/ArfinHasib/Lamp-Website-Responsive',
-    visit: 'https://arfinhasib.github.io/Lamp-Website-Responsive/',
-    id: 0,
-  },
-  {
-    title: 'Magic Match Game',
-    description:
-      'A Simple game which takes 2 similar photos and matches them. A classic game which we all used to play. And it was quite fun to play for me. Used Advanced React concepts and fucntions to build the full game.',
-    image: '/images/2.png',
-    tags: ['React', 'JavaScript', 'JSX'],
-    source: 'https://github.com/ArfinHasib/Magic-Match',
-    visit: 'https://magicandmatch.netlify.app/',
-    id: 1,
-  },
-  {
-    title: 'MyMoney Expense Tracker',
-    description:
-      'I built a simple and minimal expense tracker app using React, JSX, React hooks, Router & Firebase as a backend where you can store email and password and login again. Just use the sign up to create a new user and then add expenses list. ',
-    image: '/images/3.png',
-    tags: ['React', 'React Router', 'Firebase'],
-    source: 'https://github.com/ArfinHasib/CineMate-React',
-    visit: 'https://money-expense-trackerr.netlify.app/',
-    id: 2,
-  },
-  {
-    title: 'GameHub',
-    description:
-      'This is a code repository for the corresponding video tutorial. In this video, we will create a full Realtime Chat Application',
-    image: '/images/4.png',
-    tags: ['NextJS', 'Typescript', 'React Query', 'Zod'],
-    source: 'https://github.com/ArfinHasib/game-hub-2.0',
-    visit: 'https://game-hub-2-0.vercel.app/',
-    id: 3,
-  },
+   {
+      title: 'AI Website Landing Page',
+      description:
+         'Brainwave - Modern UI/UX website, developed using React.js and Tailwind CSS, exemplifies modern UI/UX principles. Its sleek design, seamless animations, and overall user experience set a high standard',
+      image: '/images/1.png',
+      tags: ['React', 'Tailwind', 'Vite'],
+      source: 'https://github.com/ArfinHasib/BrainWave-AI',
+      visit: 'https://github.com/ArfinHasib/BrainWave-AI',
+      id: 0,
+   },
+   {
+      title: 'Magic Match Game',
+      description:
+         'A Simple game which takes 2 similar photos and matches them. A classic game which we all used to play. And it was quite fun to play for me. Used Advanced React concepts and fucntions to build the full game.',
+      image: '/images/2.png',
+      tags: ['React', 'JavaScript', 'JSX'],
+      source: 'https://github.com/ArfinHasib/Magic-Match',
+      visit: 'https://magicandmatch.netlify.app/',
+      id: 1,
+   },
+   {
+      title: 'MyMoney Expense Tracker',
+      description:
+         'I built a simple and minimal expense tracker app using React, JSX, React hooks, Router & Firebase as a backend where you can store email and password and login again. Just use the sign up to create a new user and then add expenses list. ',
+      image: '/images/3.png',
+      tags: ['React', 'React Router', 'Firebase'],
+      source: 'https://github.com/ArfinHasib/CineMate-React',
+      visit: 'https://money-expense-trackerr.netlify.app/',
+      id: 2,
+   },
+   {
+      title: 'GameHub',
+      description:
+         'This is a code repository for the corresponding video tutorial. In this video, we will create a full Realtime Chat Application',
+      image: '/images/4.png',
+      tags: ['NextJS', 'Typescript', 'React Query', 'Zod'],
+      source: 'https://github.com/ArfinHasib/game-hub-2.0',
+      visit: 'https://game-hub-2-0.vercel.app/',
+      id: 3,
+   },
 ];
 
 export const TimeLineData = [
-  { year: 2020, text: 'Started my journey' },
-  { year: 2021, text: 'Worked as a freelance developer' },
-  { year: 2021, text: 'Masterd UI & UX Design' },
-  { year: 2022, text: 'Mastered React, Next & Typescript' },
-  { year: 2023, text: 'Started My Own Youtube Channel - Code Ninja' },
+   { year: 2020, text: 'Started my journey' },
+   { year: 2021, text: 'Worked as a freelance developer' },
+   { year: 2021, text: 'Masterd UI & UX Design' },
+   { year: 2022, text: 'Mastered React, Next & Typescript' },
+   { year: 2023, text: 'Started My Own Youtube Channel - Code Ninja' },
 ];
