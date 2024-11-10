d07c8914d876859c22ea315954af1eeceaf860f7
Arfin Hasib <arfinhasib21375@gmail.com>
2024-11-10 17:25:59 +0600

    updates

--- src/components/Acomplishments/Acomplishments.js
+++ src/components/Acomplishments/Acomplishments.js
@@ -1,32 +1,33 @@
 import React from "react";
 
 import {
-  Section,
-  SectionDivider,
-  SectionTitle,
+	Section,
+	SectionDivider,
+	SectionTitle,
 } from "../../styles/GlobalComponents";
 import { Box, Boxes, BoxNum, BoxText } from "./AcomplishmentsStyles";
 
 const data = [
-  { number: 10, text: "Open Source Projects" },
-  { number: 100, text: "Clients" },
-  { number: 200, text: "Completed Projects" },
-  // { number: 5000, text: "Github Stars" },
+	{ number: 10, text: "Open Source Projects" },
+	{ number: 100, text: "Clients" },
+	{ number: 200, text: "Completed Projects" },
+	{ number: 310, text: "YouTube Subscribers" },
+	// { number: 5000, text: "Github Stars" },
 ];
 
 const Acomplishments = () => (
-  <Section>
-    <SectionTitle>Personal Achievements</SectionTitle>
-    <Boxes>
-      {data.map((card, index) => (
-        <Box key={index}>
-          <BoxNum>{`${card.number}+`}</BoxNum>
-          <BoxText>{card.text}</BoxText>
-        </Box>
-      ))}
-    </Boxes>
-    <SectionDivider />
-  </Section>
+	<Section>
+		<SectionTitle>Personal Achievements</SectionTitle>
+		<Boxes>
+			{data.map((card, index) => (
+				<Box key={index}>
+					<BoxNum>{`${card.number}+`}</BoxNum>
+					<BoxText>{card.text}</BoxText>
+				</Box>
+			))}
+		</Boxes>
+		<SectionDivider />
+	</Section>
 );
 
 export default Acomplishments;
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,50 +1,53 @@
 export const projects = [
-   {
-      title: 'Viral Profit',
-      description:
-         'Develop the FrontEnd with React, Chakra UI, Tailwind, etc and Along with 7 other Developers Develop systems for quizzes, checkout processes, viewing online course videos, and booking calendars, working with the Slack API.',
-      image: '/images/5.png',
-      tags: ['React.js', 'Node.js', 'PostgreSQL', 'Render.com'],
-      source: 'https://www.viralprofit.yt/',
-      visit: 'https://www.viralprofit.yt/',
-      id: 0,
-   },
-   {
-      title: 'Matching Game',
-      description:
-         'A Simple game which takes 2 similar photos and matches them. A classic game which we all used to play. And it was quite fun to play for me. Used Advanced React concepts and fucntions to build the full game.',
-      image: '/images/2.png',
-      tags: ['React', 'JavaScript', 'Tailwind', "Vite"],
-      source: 'https://github.com/ArfinHasib/Magic-Match',
-      visit: 'https://magicandmatch.netlify.app/',
-      id: 1,
-   },
-   {
-      title: 'Expense Tracker',
-      description:
-         'I built a simple and minimal expense tracker app using React, JSX, React hooks, Router & Firebase as a backend where you can store email and password and login again. Just use the sign up to create a new user and then add expenses list. ',
-      image: '/images/3.png',
-      tags: ['React', 'React Router', 'Firebase', "TypeScript"],
-      source: 'https://github.com/ArfinHasib/CineMate-React',
-      visit: 'https://money-expense-trackerr.netlify.app/',
-      id: 2,
-   },
-   {
-      title: 'GameHub',
-      description:
-         'This is a code repository for the corresponding video tutorial. In this video, we will create a full Realtime Chat Application',
-      image: '/images/4.png',
-      tags: ['NextJS', 'Typescript', 'React Query', 'Zod'],
-      source: 'https://github.com/ArfinHasib/game-hub-2.0',
-      visit: 'https://game-hub-2-0.vercel.app/',
-      id: 3,
-   },
+	{
+		title: "Viral Profit",
+		description:
+			"Develop the FrontEnd with React, Chakra UI, Tailwind, etc and Along with 7 other Developers Develop systems for quizzes, checkout processes, viewing online course videos, and booking calendars, working with the Slack API.",
+		image: "/images/5.png",
+		tags: ["React.js", "Node.js", "PostgreSQL", "Render.com"],
+		source: "https://www.viralprofit.yt/",
+		visit: "https://www.viralprofit.yt/",
+		id: 0,
+	},
+	{
+		title: "Matching Game",
+		description:
+			"A Simple game which takes 2 similar photos and matches them. A classic game which we all used to play. And it was quite fun to play for me. Used Advanced React concepts and fucntions to build the full game.",
+		image: "/images/2.png",
+		tags: ["React", "JavaScript", "Tailwind", "Vite"],
+		source: "https://github.com/ArfinHasib/Magic-Match",
+		visit: "https://magicandmatch.netlify.app/",
+		id: 1,
+	},
+	{
+		title: "Expense Tracker",
+		description:
+			"I built a simple and minimal expense tracker app using React, JSX, React hooks, Router & Firebase as a backend where you can store email and password and login again. Just use the sign up to create a new user and then add expenses list. ",
+		image: "/images/3.png",
+		tags: ["React", "React Router", "Firebase", "TypeScript"],
+		source: "https://github.com/ArfinHasib/CineMate-React",
+		visit: "https://money-expense-trackerr.netlify.app/",
+		id: 2,
+	},
+	{
+		title: "GameHub",
+		description:
+			"This is a code repository for the corresponding video tutorial. In this video, we will create a full Realtime Chat Application",
+		image: "/images/4.png",
+		tags: ["NextJS", "Typescript", "React Query", "Zod"],
+		source: "https://github.com/ArfinHasib/game-hub-2.0",
+		visit: "https://game-hub-2-0.vercel.app/",
+		id: 3,
+	},
 ];
 
 export const TimeLineData = [
-   { year: 2020, text: 'Started my journey' },
-   { year: 2021, text: 'Worked as a freelance developer' },
-   { year: 2021, text: 'Masterd UI & UX Design' },
-   { year: 2022, text: 'Mastered React, Next & Typescript' },
-   { year: 2023, text: 'Started My Own Youtube Channel - Code Ninja' },
+	{ year: 2018, text: "Started my journey with Html, CSS and JavaScript" },
+	{ year: 2019, text: "Started working as a freelance developer" },
+	{ year: 2021, text: "Masterd UI & UX Design, Webflow & Wordpress" },
+	{
+		year: 2023,
+		text: "Mastered React, NextJs, Tailwind, & Typescript",
+	},
+	{ year: 2024, text: "Started My Own Youtube Channel - DevLab Tutorials" },
 ];
